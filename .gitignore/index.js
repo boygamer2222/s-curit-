fc99b15f68d4b42cfa40725a6535c87c4b5b71ac
boygamer2222 <53156276+boygamer2222@users.noreply.github.com>
2019-07-22 11:43:48 -0700

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,14 +1,23 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+var prefix = ("*")
+
+bot.on('ready', function() {
+  bot.user.setGame("Command: *help");
+  console.log("connectedç");
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+bot.login('NjAwNjY2MjUzMTQwNDI2NzUy.XTXq2g.yc20h_nwqTtNWQT6OvnGELMHHHs');
+
+
+bot.on('messade', message => {
+  if (message.content === prefix + "help"){
+    message.channel.sendMessage("Liste des commandes: \n -*help");
+  }
+
+  if (message.content === "Salut"){
+    message.reply("Bien le bonjour. :)");
+    console.log("commande Salut effectué");
   }
 });
-
-client.login('NjAwNjY2MjUzMTQwNDI2NzUy.XTXq2g.yc20h_nwqTtNWQT6OvnGELMHHHs');
